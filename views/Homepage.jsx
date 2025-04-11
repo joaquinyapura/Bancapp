39a25e17ca0771d2d879e1ebe3710f6fc1d42694
Joaquin <joaquinyapura@gmail.com>
2025-04-11 11:58:13 -0300

    actualización de botones en página main

--- views/Homepage.jsx
+++ views/Homepage.jsx
@@ -1,4 +1,8 @@
 import { SlReload } from "react-icons/sl";
+import { BiTransfer } from "react-icons/bi";
+import { IoWalletOutline } from "react-icons/io5";
+
+import { GrMoney } from "react-icons/gr";
 
 export const Homepage = () => {
   return (
@@ -6,32 +10,74 @@ export const Homepage = () => {
       <div className="w-full h-[700px] bg-gray-100 p-6">
         <h1 className="text-4xl font-bold">Hello, Cliente Nombre!</h1>
         <div className="grid grid-cols-3 grid-rows-2 gap-2 mt-6">
-          <div className="col-span-1 row-span-2  rounded-md h-[200px] bg-white">
+          <div className="col-span-1 row-span-2  rounded-md h-[200px] bg-white  shadow-md">
             <div className="p-4">
               <h2>Cuenta única</h2>
               <p className="text-gray-600">123123-123123-23</p>
               <p className="text-3xl font-bold mb-2">$102.344.123,23</p>
               <p className="text-3xl font-bold mb-2">U$S 13,23</p>
-              <div className="flex justify-between text-md">
-                <div className="flex items-center">
+              <div className="flex justify-between text-sm">
+                <a
+                  href="#"
+                  className="flex items-center hover:text-sky-900 text-gray-700"
+                >
                   <SlReload />
-                  <p>ver movimientos</p>
-                </div>
-                <div className="flex items-center">
+                  <p className=" ml-4 font-bold">ver movimientos</p>
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center hover:text-sky-900 text-gray-700"
+                >
                   <SlReload />
-                  <p>CBU/Alias</p>
-                </div>
+                  <p className=" ml-4 font-bold">CBU/Alias</p>
+                </a>
               </div>
             </div>
           </div>
-          <div className="col-span-2 row-span-2 bg-sky-500">
-            <div className="grid grid-cols-3 gap-2 ">
-              <div className="bg-sky-200">ITEM</div>
-              <div className="bg-sky-200">ITEM</div>
-              <div className="bg-sky-200">ITEM</div>
-              <div className="bg-sky-200">ITEM</div>
-              <div className="bg-sky-200">ITEM</div>
-              <div className="bg-sky-200">ITEM</div>
+          <div className="col-span-2">
+            <div className="grid grid-cols-3 grid-rows-2 gap-2 ">
+              <a
+                href="#"
+                className="shadow bg-white rounded-md flex items-center justify-center p-6 py-9 "
+              >
+                <BiTransfer className="text-sky-400 mr-2 text-2xl" />
+                <p>Transferencias</p>
+              </a>
+              <a
+                href="#"
+                className="shadow bg-white rounded-md flex items-center justify-center p-6 py-9 "
+              >
+                <IoWalletOutline className="text-sky-400 mr-2 text-2xl" />
+                <p>Pago de servicios</p>
+              </a>
+              <a
+                href="#"
+                className="shadow bg-white rounded-md flex items-center justify-center p-6 py-9 "
+              >
+                <GrMoney className="text-sky-400 mr-2 text-2xl" />
+                <p>Prestamos</p>
+              </a>
+              <a
+                href="#"
+                className="shadow bg-white rounded-md flex items-center justify-center p-6 py-9 "
+              >
+                <BiTransfer className="text-sky-400 mr-2 text-2xl" />
+                <p>Transferencias</p>
+              </a>
+              <a
+                href="#"
+                className="shadow bg-white rounded-md flex items-center justify-center p-6 py-9 "
+              >
+                <BiTransfer className="text-sky-400 mr-2 text-2xl" />
+                <p>Transferencias</p>
+              </a>
+              <a
+                href="#"
+                className="shadow bg-white rounded-md flex items-center justify-center p-6 py-9 "
+              >
+                <BiTransfer className="text-sky-400 mr-2 text-2xl" />
+                <p>Transferencias</p>
+              </a>
             </div>
           </div>
         </div>
